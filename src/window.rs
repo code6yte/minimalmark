c182e620f52686babff3c1ddf351078cf3943802
code6yte <code6yte@users.noreply.github.com>
2026-05-17 11:53:24 +0500

    Fix: use direct assignment for RefCell

--- src/window.rs
+++ src/window.rs
@@ -313,7 +313,7 @@ impl MainWindow {
     }
 
     pub fn set_current_file(&self, path: Option<String>) {
-        self.current_file.borrow_mut().replace(path);
+        *self.current_file.borrow_mut() = path;
     }
 
     pub fn trigger_save(&self) {
